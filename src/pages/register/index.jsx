390f09d9f449f38f8c86522d5faecd5337324f1c
Caio Barbosa <caio.bar@gmail.com>
2023-12-31 16:24:42 -0300

    adding register page

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,5 +1,5 @@
 import { useNavigate  } from "react-router-dom";
-import { MdEmail, MdLock } from 'react-icons/md'
+import { MdEmail, MdLock, MdPersonPin } from 'react-icons/md'
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
@@ -8,14 +8,14 @@ import { api } from '../../services/api';
 import { useForm } from "react-hook-form";
 
 
-import { Container, Title, Column, TitleLogin, SubtitleLogin, EsqueciText, CriarText, Row, Wrapper } from './styles';
+import { Container, Title, Column, TitleLogin, SubtitleLogin, SubtitleDoLogin, SubtitleDoLogin2, RowDown, CriarText, Row, Wrapper } from './styles';
 
-const Login = () => {
+const Register = () => {
 
     const navigate = useNavigate()
 
-    const handleClickRegister = () => {
-        navigate('/register')
+    const handleClickSignIn = () => {
+        navigate('/login')
     }
 
     const { control, handleSubmit, formState: { errors  } } = useForm({
@@ -25,14 +25,7 @@ const Login = () => {
 
     const onSubmit = async (formData) => {
         try{
-            const {data} = await api.get(`/users?email=${formData.email}&senha=${formData.senha}`);
-            
-            if(data.length && data[0].id){
-                navigate('/feed') 
-                return
-            }
-
-            alert('Usuário ou senha inválido')
+            navigate('/feed')
         }catch(e){
             //TODO: HOUVE UM ERRO
         }
@@ -52,20 +45,28 @@ const Login = () => {
                 <TitleLogin>Faça seu cadastro</TitleLogin>
                 <SubtitleLogin>Faça seu login e make the change._</SubtitleLogin>
                 <form onSubmit={handleSubmit(onSubmit)}>
+                    <Input placeholder="Nome completo" leftIcon={<MdPersonPin />} name="nome"  control={control} />
+                    {errors.nome && <span>Nome completo é obrigatório</span>}
                     <Input placeholder="E-mail" leftIcon={<MdEmail />} name="email"  control={control} />
                     {errors.email && <span>E-mail é obrigatório</span>}
                     <Input type="password" placeholder="Senha" leftIcon={<MdLock />}  name="senha" control={control} />
                     {errors.senha && <span>Senha é obrigatório</span>}
-                    <Button title="Entrar" variant="secondary" type="submit"/>
+                    <Button title="Criar minha conta" variant="secondary" type="submit"/>
                 </form>
                 <Row>
-                    <EsqueciText>Esqueci minha senha</EsqueciText>
-                    <CriarText onClick={handleClickRegister}>Criar Conta</CriarText>
+                    <SubtitleDoLogin>
+                        Ao clicar em "criar minha conta grátis", declaro que aceitos as Políticas de Privacidade e os Termos
+                        de Uso da DIO.
+                    </SubtitleDoLogin>
                 </Row>
+                <RowDown>
+                    <SubtitleDoLogin2>já tenho conta.</SubtitleDoLogin2>
+                    <CriarText  onClick={handleClickSignIn}>Fazer login</CriarText>
+                </RowDown>
                 </Wrapper>
             </Column>
         </Container>
     </>)
 }
 
-export { Login }
+export { Register }
